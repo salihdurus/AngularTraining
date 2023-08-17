34b10801d690251955a8ecb5bd67586886ffcb6c
salihdurus <durussalih@gmail.com>
2023-08-17 10:03:40 +0300

    ngIf and ngFor learned

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,16 +3,16 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent {
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("Salih", "Dürüs", "asda@gmail.com", 156),
-    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 156),
-    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 156),
-    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 156),
-    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 156),
-    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 156),
+    new SalesPerson("Salih", "Dürüs", "asda@gmail.com", 150),
+    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 250),
+    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 350),
+    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 450),
+    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 550),
+    new SalesPerson("Ali", "Dürüs", "asda@gmail.com", 650),
   ];
 }
